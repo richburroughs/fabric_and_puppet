d8ad005786cf649259b717183166be40f2edf62a
Rich Burroughs <richard.burroughs@yesmail.com>
2016-03-30 13:11:26 -0700

    added roles file

---
+++ roles.py
@@ -0,0 +1,12 @@
+# Import all Fabric APIs
+from fabric.api import *
+
+# Define get_roles function, not a Fabric function
+def get_roles(*roles):
+    return lambda:[y for x in roles for y in env.roledefs[x]]
+
+env.roledefs = {
+        'web': ['agent1'],
+        'db': ['agent2'],
+        'all': get_roles('web', 'db')
+}
